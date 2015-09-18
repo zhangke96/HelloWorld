1fca82abd1224d25da7db22d2f11739a1ab9d939
zhangke <zhangke960808@163.com>
2015-09-18 15:40:27 +0800

    It's my first time to use git

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: HelloWorld.cpp
+    > Author: zhangke
+    > Mail: zhangke960808@163.com 
+    > Created Time: 2015年09月18日 星期五 15时39分28秒
+ ************************************************************************/
+
+#include<iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+	cout<<"HelloWorld"<<endl;
+	return 0;
+}
